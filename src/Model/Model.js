8a33c4b71bfd5b413fbbfc09a71a429659630c3a
Egor Vasyakin <e.vasyakin@itevas.ru>
2023-06-08 09:09:07 +0300

    Update Model.js

--- src/Model/Model.js
+++ src/Model/Model.js
@@ -1,29 +1,78 @@
+/**
+ * Data Model.
+ * @package evas-vue-core
+ * @author Egor Vasyakin <egor@evas-php.com>
+ * @license CC-BY-4.0
+ */
 
+import { Query } from '../Query.js'
+
+export class Model {
+    constructor (data, afterFetch = true) {
+        console.log('constructor', data)
+        this.$beforeNew(data, afterFetch)
+        if (!data) data = {}
+        this.$fill(data)
+        if (!afterFetch || this.$id) {
+            this.constructor.insertOrUpdate(this, afterFetch)
+        }
+        return new Proxy(this, this)
+    }
+
+    static entityName = null
+    static primary = 'id'
+}
+
+Model.isRootModel = function () {
+    return this.name === 'Model'// || this.entityName === null
+}
+
+// Model.entityName = null
+// Model.primary = 'id'
+
+// entity $id (value of the primary key)
+Object.defineProperty(Model.prototype, '$id', {
+    set: function (value) {
+        this[this.constructor.primary] = value
+    },
+    get: function () {
+        return this[this.constructor.primary]
+    }
+})
+
+Model.prototype.$fill = function (data) {
+    this.constructor.eachFields((field) => {
+        this[field.name] = field.convertType(undefined !== data[field.name]
+            ? data[field.name]
+            : field.default)
+    })
+    this.constructor.eachRelations((field) => {
+        this[field.name] = undefined !== data[field.name]
+            ? data[field.name]
+            : field.default
+        field.foreignModel.insertOrUpdate(data[field.name], true)
+    })
+}
+
+// Model hooks
+Model.prototype.$beforeNew = function () {}
+
+require('./Model.api.js')
+require('./Model.crud.js')
 require('./Model.fields.js')
 require('./Model.relations.js')
 require('./Model.state.js')
+require('./Model.store.js')
 require('./Model.validate.js')
 
-// import { addFields } from './Model.fields.js'
-
-export function Model() {
-    console.log('init Model')
+// Model query
+Model.query = function () {
+    return new Query(this)
 }
-
-// Model.addFields = addFields
-
-// export class Model {
-//     constructor() {
-//         console.log('init Model')
-//     }
-// }
-
-Model.isRootModel = function () {
-    return this.name === 'Model'
-}
-
-Model.prototype.name1 = 'test'
-
-Model.prototype.hello = function () {
-    console.log('hello')
+Model.find = function (id) {
+    let query = this.query()
+    // if (arguments.length > 1 && !Array.isArray(id)) id = arguments
+    return Array.isArray(id)
+        ? query.whereIn(this.primary, id).get()
+        : query.where(this.primary, id).first()
 }
