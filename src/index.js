afca60fa9d9efd4e3c898da13864195b548ffa27
Egor Vasyakin <e.vasyakin@itevas.ru>
2023-06-08 09:09:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,39 +5,39 @@
  * @license CC-BY-4.0
  */
 
-// import { reactive } from 'vue'
-// import { Api } from './Api.js'
+import { Api } from './Api.js'
+import { reactive } from 'vue'
 
 export { Model } from './Model/Model.js'
 // export { MockApi } from './MockApi.js'
 
-// export const EvasVue = new function () {
-//     this.models = reactive({})
-//     this.api = null
+export const EvasVue = new function () {
+    this.models = reactive({})
+    this.api = null
 
-//     this.install = (app, options) => {
-//         if (options) {
-//             if (options.api) this.setApi(options.api)
-//             if (options.models) this.setModels(options.models)
-//         }
-//         app.config.globalProperties.$models = this.models
-//         app.config.globalProperties.$api = this.api
-//     }
+    this.install = (app, options) => {
+        if (options) {
+            if (options.api) this.setApi(options.api)
+            if (options.models) this.setModels(options.models)
+        }
+        app.config.globalProperties.$models = this.models
+        app.config.globalProperties.$api = this.api
+    }
 
-//     this.setApi = (api) => {
-//         this.api = api instanceof Api ? api : new Api(api)
-//         if (this.models) this.setModels(Object.values(this.models))
-//     }
+    this.setApi = (api) => {
+        this.api = api instanceof Api ? api : new Api(api)
+        if (this.models) this.setModels(Object.values(this.models))
+    }
 
-//     this.setModels = (models) => {
-//         Object.entries(models).forEach(([name, model]) => {
-//             if (this.api) model.setApi(this.api)
-//             model.entityName = name
-//             this.models[name] = model
-//         })
-//     }
+    this.setModels = (models) => {
+        Object.entries(models).forEach(([name, model]) => {
+            if (this.api) model.setApi(this.api)
+            model.entityName = name
+            this.models[name] = model
+        })
+    }
 
-//     this.getModel = (modelName) => {
-//         return this.models[modelName]
-//     }
-// }
+    this.getModel = (modelName) => {
+        return this.models[modelName]
+    }
+}
