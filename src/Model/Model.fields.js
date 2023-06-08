164e91c63fd7b8c9d264d0dae25bd57f59f7982f
Egor Vasyakin <e.vasyakin@itevas.ru>
2023-06-08 09:08:37 +0300

    Update Model.fields.js

--- src/Model/Model.fields.js
+++ src/Model/Model.fields.js
@@ -7,24 +7,29 @@
 
 import { Model } from './Model.js'
 import { Field } from '../Field/Field.js'
+import { FieldBuilder } from '../Field/FieldBuilder.js'
 
-Model.addFields = function () {
+Model.setFields = function () {
     return []
 }
 
 Model.fields = function () {
-    if (this.isRootModel()) return []
+    if (this.isRootModel()) return {}
     if (!this._fields) {
-        this._fields = new Field
-        console.log(this._fields.name('123').min(3).max(10))
-        // let fields = this.addFields()
-        // for (let name in fields) {
-        //     let field = fields[name]
-        //     if (field instanceof Field) {
-        //         field.name(name)
-        //         this._fields[name] = field
-        //     }
-        // }
+        this._fields = {}
+        // this._fields = new Field
+        // console.log(this._fields.name('123').min(3).max(10))
+        let fields = this.setFields()
+        for (let name in fields) {
+            let field = fields[name]
+            if (field instanceof FieldBuilder) {
+                field = new Field(field.export())
+            }
+            if (field instanceof Field) {
+                field.name = name
+                this._fields[name] = field
+            }
+        }
     }
     return this._fields
 }
@@ -53,14 +58,14 @@ Model.eachFields = function (cb, names) {
 
 // set fields
 Model.attr = function (_default) {
-    return new Field({ _default })
+    return new FieldBuilder({ _default })
 }
 Model.number = function (_default) {
-    return new Field({ _default })
+    return new FieldBuilder({ _default, _type: 'number' })
 }
 Model.string = function (_default) {
-    return new Field({ _default })
+    return new FieldBuilder({ _default, _type: 'string' })
 }
 Model.boolean = function (_default) {
-    return new Field({ _default })
+    return new FieldBuilder({ _default, _type: 'boolean' })
 }
