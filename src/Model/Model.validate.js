420e173593029254a6dead37c7ec843ccbaf88a2
Egor Vasyakin <e.vasyakin@itevas.ru>
2023-06-08 09:08:54 +0300

    Update Model.validate.js

--- src/Model/Model.validate.js
+++ src/Model/Model.validate.js
@@ -7,49 +7,61 @@
 
 import { Model } from './Model.js'
 
-Model.defaultValidateErrorHandler = null
-// Model.validateErrorHandlers = {}
+/** @var Function обработчик ошибок валидации */
+Model.validateErrorHandler = null
 
-Model.setDefaultValidateErrorHandler = function (cb) {
+/**
+ * Установка обработчика ошибок валидации.
+ * @param Function
+ * @return self
+ */
+Model.setValidateErrorHandler = function (cb) {
     if ('function' !== typeof cb) throw new Error(
         `default validate error handler must be a function, ${typeof cb} given`
     )
-    this.defaultValidateErrorHandler = cb
+    this.validateErrorHandler = cb
     return this
 }
 
-// Model.setValidateErrorHandler = function (fieldName, cb) {
-//     if (!['string', 'number'].includes(typeof fieldName)) throw new Error(
-//         `validate error handler field name must a string, ${typeof fieldName} given`
-//     )
-//     if ('function' !== typeof cb) throw new Error(
-//         `validate error handler for field "${fieldName}" must be a function, ${typeof cb} given`
-//     )
-//     this.validateErrorHandlers[fieldName] = cb
-//     return this
-// }
-
+/**
+ * Обработка ошибки валидации.
+ */
 Model.handleValidateError = function (field, error) {
-    // if (this.validateErrorHandlers[field._name]) {
-    //     this.validateErrorHandlers[field._name](error)
-    // } else 
-    if (this.defaultValidateErrorHandler) {
-        this.defaultValidateErrorHandler(error, field._name)
+    if (this.validateErrorHandler) {
+        this.validateErrorHandler(error, field.name)
     } else {
         console.error(field, error)
     }
 }
 
+/** @var Array ошибки валидации */
 Model.prototype.$errors = []
 
+/**
+ * Валидация записи.
+ */
 Model.prototype.$validate = function () {
     this.$errors = []
     this.constructor.eachFields((field) => {
-        // field.throwIfNotValid(this[field._name])
-        if (!field.isValid(this[field._name])) {
+        if (!field.isValid(this[field.name])) {
             this.constructor.handleValidateError(field, field.error)
             this.$errors.push(field.error)
         }
-    }, this.dirtyFields())
+    }, this.$dirtyFields())
     return this.$errors.length < 1
+}
+
+/**
+ * Валидация записи/записей.
+ */
+Model.validate = function (entity) {
+    if (Array.isArray(entity)) {
+        entity.forEach((_entity) => this.validate(_entity))
+    }
+    else if (entity && entity instanceof this) {
+        return entity.$validate()
+    }
+    else if (entity && 'object' === typeof entity) {
+        return (new this(entity)).$validate()
+    }
 }
