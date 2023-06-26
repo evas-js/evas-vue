317fc11cffc0fa2b5c2ec24cc5752b648818454c
Egor Vasyakin <e.vasyakin@itevas.ru>
2023-06-27 02:44:25 +0300

    Update ModelsStore.js

--- src/ModelsStore.js
+++ src/ModelsStore.js
@@ -5,12 +5,18 @@
  * @license CC-BY-4.0
  */
 
-import { Model } from './Model/Model.js'
+// import { Model } from './Model/Model.js'
 import { reactive } from 'vue'
 
 export class ModelsStoreConstructor {
+    /** @var Proxy Map'ы по именам моделей */
     map = reactive({})
 
+    /**
+     * Получение Map записей модели.
+     * @param String имя модели
+     * @return Map
+     */
     model(name) {
         if (!this.map[name]) {
             this.map[name] = new Map
@@ -19,33 +25,77 @@ export class ModelsStoreConstructor {
     }
 
 
-    set(name, entities) {
-        let model = this.model(name)
-        if (!Array.isArray(entities)) entities = [entities]
-        entities.forEach(entity => {
-            if (!(entity instanceof Model)) {
-                throw new Error('ModelsStore: entity must be instanceof Model')
-            }
-            model.set(entity.$id, entity)
-        })
-    }
+    // /**
+    //  * Добавление записи/записей в Map записей модели.
+    //  * @param String имя модели
+    //  * @param Model|Array запись или массив записей
+    //  * @return this
+    //  */
+    // set(name, entities) {
+    //     let model = this.model(name)
+    //     if (!Array.isArray(entities)) entities = [entities]
+    //     entities.forEach(entity => {
+    //         if (!(entity instanceof Model)) {
+    //             throw new Error('ModelsStore: entity must be instanceof Model')
+    //         }
+    //         model.set(entity.$id, entity)
+    //     })
+    //     return this
+    // }
 
-    delete(name, id) {
-        this.model(name).delete(id)
-    }
+    // /**
+    //  * Удаление записи по id из Map заисей модели.
+    //  * @param String имя модели
+    //  * @param String|Number id записи
+    //  * @return this
+    //  */
+    // delete(name, id) {
+    //     this.model(name).delete(id)
+    //     return this
+    // }
 
-    get(name, id) {
-        return this.model(name).get(id)
-    }
+    // /**
+    //  * Получение записи по id из Map записей модели.
+    //  * @param String имя модели
+    //  * @param String|Number id записи
+    //  * @return Model 
+    //  */
+    // get(name, id) {
+    //     return this.model(name).get(id)
+    // }
 
+    // /**
+    //  * Очистка Map записей модели.
+    //  * @param String имя модели
+    //  * @return this
+    //  */
+    // clear(name) {
+    //     this.model(name).clear()
+    //     return this
+    // }
+
+
+    /** @var Proxy totalRows'ы по именам модели */
     _totalRows = reactive({})
 
+    /**
+     * Получение totalRows модели.
+     * @param String имя модели
+     * @return Number
+     */
     totalRows(name) {
         return this._totalRows[name] || 0
     }
 
+    /**
+     * Установка totalRows модели.
+     * @param String имя модели
+     * @param Number totalRows
+     * @return this
+     */
     setTotalRows(name, value) {
         this._totalRows[name] = value
+        return this
     }
 }
 
