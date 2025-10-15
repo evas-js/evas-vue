aa07e0adad178525ae24398a7e44f07869767552
prodvair <prodvair.almaz@ya.ru>
2025-10-15 21:55:02 +0300

    Исправлено формотриование

--- src/Model/Model.state.js
+++ src/Model/Model.state.js
@@ -5,70 +5,70 @@
  * @license CC-BY-4.0
  */
 
-import { logger } from "../Log.js";
-import { Model } from "./Model.js";
-import { Relation } from "../Relation.js";
+import { logger } from '../Log.js'
+import { Model } from './Model.js'
+import { Relation } from '../Relation.js'
 
-Model.prototype.$state = {};
+Model.prototype.$state = {}
 
 /**
  * Сохранение состояния записи.
  */
 Model.prototype.$saveState = function () {
-  this.$state = structuredClone(this);
-  delete this.$state.$state;
-  delete this.$state.$displayGroup;
-  delete this.$state.$errors;
-  logger.methodCall(`${this.$entityNameWithId}.$saveState`, arguments);
-};
+    this.$state = structuredClone(this)
+    delete this.$state.$state
+    delete this.$state.$displayGroup
+    delete this.$state.$errors
+    logger.methodCall(`${this.$entityNameWithId}.$saveState`, arguments)
+}
 
 /**
  * Откат изменений записи.
  * @param Array|null имена полей и/или связей
  */
 Model.prototype.$rollbackChanges = function (names) {
-  const cb = (field) => {
-    this[field.name] = structuredClone(this.$state[field.name]);
-  };
-  this.constructor.eachFields(cb, names);
-  this.constructor.eachRelations(cb, names);
-};
+    const cb = field => {
+        this[field.name] = structuredClone(this.$state[field.name])
+    }
+    this.constructor.eachFields(cb, names)
+    this.constructor.eachRelations(cb, names)
+}
 
 /**
  * @var Boolean Является ли запись новой.
  */
-Object.defineProperty(Model.prototype, "$isNew", {
-  get: function () {
-    return this.$id ? false : true;
-  },
-});
+Object.defineProperty(Model.prototype, '$isNew', {
+    get: function () {
+        return this.$id ? false : true
+    },
+})
 
 /**
  * @var Boolean Является ли запись "грязной" (с изменёнными, но не сохранёнными данными)
  */
-Object.defineProperty(Model.prototype, "$isDirty", {
-  get: function () {
-    return this.$isDirtyData || this.$isDirtyRelations;
-  },
-});
+Object.defineProperty(Model.prototype, '$isDirty', {
+    get: function () {
+        return this.$isDirtyData || this.$isDirtyRelations
+    },
+})
 
 /**
  * @var Boolean Является ли связанная Модель с записми "грязной" (с изменёнными, но не сохранёнными данными)
  */
-Object.defineProperty(Model.prototype, "$isDirtyData", {
-  get: function () {
-    return this.$dirtyFields().length > 0;
-  },
-});
+Object.defineProperty(Model.prototype, '$isDirtyData', {
+    get: function () {
+        return this.$dirtyFields().length > 0
+    },
+})
 
 /**
  * @var Boolean Является ли связанная Модель с записми "грязной" (с изменёнными, но не сохранёнными данными)
  */
-Object.defineProperty(Model.prototype, "$isDirtyRelations", {
-  get: function () {
-    return this.$dirtyRelations().length > 0;
-  },
-});
+Object.defineProperty(Model.prototype, '$isDirtyRelations', {
+    get: function () {
+        return this.$dirtyRelations().length > 0
+    },
+})
 
 /**
  * Проверка поля на изменённость.
@@ -76,20 +76,17 @@ Object.defineProperty(Model.prototype, "$isDirtyRelations", {
  * @return Boolean
  */
 Model.prototype.$isDirtyField = function (name) {
-  let stateValue = this.$state[name];
-  let value = this[name];
-  if (Array.isArray(stateValue) && Array.isArray(value)) {
-    return JSON.stringify(stateValue) !== JSON.stringify(value);
-  } else if (
-    typeof stateValue === "object" &&
-    ![null, undefined].includes(stateValue)
-  ) {
-    return JSON.stringify(stateValue) !== JSON.stringify(value);
-  } else if ([null, undefined].includes(stateValue) && value === "") {
-    return false;
-  }
-  return stateValue !== value;
-};
+    let stateValue = this.$state[name]
+    let value = this[name]
+    if (Array.isArray(stateValue) && Array.isArray(value)) {
+        return JSON.stringify(stateValue) !== JSON.stringify(value)
+    } else if (typeof stateValue === 'object' && ![null, undefined].includes(stateValue)) {
+        return JSON.stringify(stateValue) !== JSON.stringify(value)
+    } else if ([null, undefined].includes(stateValue) && value === '') {
+        return false
+    }
+    return stateValue !== value
+}
 
 /**
  * Проверка связанных записей на изменённость.
@@ -97,11 +94,9 @@ Model.prototype.$isDirtyField = function (name) {
  * @return Boolean
  */
 Model.prototype.$isDirtyRelateds = function (relation) {
-  if (!(relation instanceof Relation)) relation = this.relation()[relation];
-  return [this[relation.name]]
-    .flat()
-    .some((relationEntity) => relationEntity.$isDirty);
-};
+    if (!(relation instanceof Relation)) relation = this.relation()[relation]
+    return [this[relation.name]].flat().some(relationEntity => relationEntity.$isDirty)
+}
 
 /**
  * Получение имён изменённых полей и связанных записей.
@@ -109,12 +104,12 @@ Model.prototype.$isDirtyRelateds = function (relation) {
  * @return String[]
  */
 Model.prototype.$dirtyFields = function (names) {
-  let dirty = [];
-  this.constructor.eachFields((field) => {
-    if (this.$isDirtyField(field.name)) dirty.push(field.name);
-  }, names);
-  return dirty;
-};
+    let dirty = []
+    this.constructor.eachFields(field => {
+        if (this.$isDirtyField(field.name)) dirty.push(field.name)
+    }, names)
+    return dirty
+}
 
 /**
  * Получение имён изменённых полей и связанных записей.
@@ -122,32 +117,29 @@ Model.prototype.$dirtyFields = function (names) {
  * @return String[]
  */
 Model.prototype.$dirtyRelations = function (names) {
-  let dirty = [];
-  this.constructor.eachRelations((relation) => {
-    if (this.$isDirtyRelateds(relation)) dirty.push(relation.name);
-  }, names);
-  return dirty;
-};
+    let dirty = []
+    this.constructor.eachRelations(relation => {
+        if (this.$isDirtyRelateds(relation)) dirty.push(relation.name)
+    }, names)
+    return dirty
+}
 
 /**
  * @var Object изменённые данные {key: val, ...}
  */
-Object.defineProperty(Model.prototype, "$dirtyData", {
-  get: function () {
-    let data = {};
-    this.$dirtyFields().forEach((name) => {
-      let relation = this.constructor.relation(name);
-      if (relation) {
-        if (relation.multiple && Array.isArray(this[name])) {
-          data[name] = this[name]
-            .map(
-              (relationEntity) =>
-                relationEntity.$isDirty && relationEntity.$dirtyData
-            )
-            .filter((data) => data);
-        } else data[name] = this[name].$dirtyData;
-      } else data[name] = this[name];
-    });
-    return data;
-  },
-});
+Object.defineProperty(Model.prototype, '$dirtyData', {
+    get: function () {
+        let data = {}
+        this.$dirtyFields().forEach(name => {
+            let relation = this.constructor.relation(name)
+            if (relation) {
+                if (relation.multiple && Array.isArray(this[name])) {
+                    data[name] = this[name]
+                        .map(relationEntity => relationEntity.$isDirty && relationEntity.$dirtyData)
+                        .filter(data => data)
+                } else data[name] = this[name].$dirtyData
+            } else data[name] = this[name]
+        })
+        return data
+    },
+})
--- src/Model/Model.validate.js
+++ src/Model/Model.validate.js
@@ -5,13 +5,13 @@
  * @license CC-BY-4.0
  */
 
-import { logger } from "../Log.js";
-import { Model } from "./Model.js";
-import { Field } from "../Field/Field.js";
-import { VariableField } from "../Field/VariableField.js";
+import { logger } from '../Log.js'
+import { Model } from './Model.js'
+import { Field } from '../Field/Field.js'
+import { VariableField } from '../Field/VariableField.js'
 
 /** @var Function обработчик ошибок валидации */
-Model.validateErrorHandler = null;
+Model.validateErrorHandler = null
 
 /**
  * Установка обработчика ошибок валидации.
@@ -19,149 +19,140 @@ Model.validateErrorHandler = null;
  * @return self
  */
 Model.setValidateErrorHandler = function (cb) {
-  if ("function" !== typeof cb)
-    throw new Error(
-      `default validate error handler must be a function, ${typeof cb} given`
-    );
-  this.validateErrorHandler = cb;
-  return this;
-};
+    if ('function' !== typeof cb)
+        throw new Error(`default validate error handler must be a function, ${typeof cb} given`)
+    this.validateErrorHandler = cb
+    return this
+}
 
 /**
  * Обработка ошибки валидации.
  */
 Model.handleValidateError = function (field, error) {
-  if (this.validateErrorHandler) {
-    this.validateErrorHandler(error, field.name);
-  } else {
-    console.error(field, error);
-  }
-};
+    if (this.validateErrorHandler) {
+        this.validateErrorHandler(error, field.name)
+    } else {
+        console.error(field, error)
+    }
+}
 
 /** @var Array ошибки валидации */
-Model.prototype.$errors = [];
+Model.prototype.$errors = []
 
 /**
  * Очистка ошибок крайней валидации.
  */
 Model.prototype.$clearErrors = function () {
-  this.$errors = [];
-};
+    this.$errors = []
+}
 
 /**
  * Получение обязательных полей
  * @returns { Object }
  */
 Model.requiredFields = function () {
-  return Object.fromEntries(
-    Object.entries(this.fields()).filter(([name, field]) => !!field.required)
-  );
-};
+    return Object.fromEntries(
+        Object.entries(this.fields()).filter(([name, field]) => !!field.required)
+    )
+}
 /**
  * Получение имён обязательных полей
  * @returns { Array }
  */
 Model.requiredFieldNames = function () {
-  return Object.keys(this.requiredFields());
-};
+    return Object.keys(this.requiredFields())
+}
 
 /**
  * Получение имен полей, которые поменялись и показываются.
  */
 Model.prototype.$fieldNamesForValidate = function () {
-  const dirty = this.$dirtyFields();
-  // const rules = this.$applyFieldsDisplayRules()
-  const display = this.$displayFields();
-  const required = this.constructor.requiredFieldNames();
-  logger.keyValue("dirty", dirty);
-  logger.keyValue("display", display);
-  logger.keyValue("required", required);
-  logger.keyValue("this", this);
-  logger.keyValue("this.$state", this.$state);
-  logger.keyValue("this.$isNew", this.$isNew);
-  let fieldNames = this.$isNew
-    ? display.concat(dirty)
-    : display.filter((name) => required.includes(name) || dirty.includes(name));
-  if (Array.isArray(this.constructor.alwaysSend)) {
-    logger.keyValue("alwaysSend", this.constructor.alwaysSend);
-    fieldNames = fieldNames.concat(this.constructor.alwaysSend);
-  }
-  // logger.keyValue('fieldNames', fieldNames)
+    const dirty = this.$dirtyFields()
+    // const rules = this.$applyFieldsDisplayRules()
+    const display = this.$displayFields()
+    const required = this.constructor.requiredFieldNames()
+    logger.keyValue('dirty', dirty)
+    logger.keyValue('display', display)
+    logger.keyValue('required', required)
+    logger.keyValue('this', this)
+    logger.keyValue('this.$state', this.$state)
+    logger.keyValue('this.$isNew', this.$isNew)
+    let fieldNames = this.$isNew
+        ? display.concat(dirty)
+        : display.filter(name => required.includes(name) || dirty.includes(name))
+    if (Array.isArray(this.constructor.alwaysSend)) {
+        logger.keyValue('alwaysSend', this.constructor.alwaysSend)
+        fieldNames = fieldNames.concat(this.constructor.alwaysSend)
+    }
+    // logger.keyValue('fieldNames', fieldNames)
 
-  // const registered = this.constructor.fieldNames()
-  // const viewRegistered = this.constructor.viewFieldNames()
-  // const diff = registered.filter(fieldName => !viewRegistered.includes(fieldName));
-  // fieldNames = fieldNames.concat(diff)
-  // logger.keyValue('registered', registered)
-  // logger.keyValue('viewRegistered', viewRegistered)
-  // logger.keyValue('diff', diff)
-  logger.keyValue("fieldNames", fieldNames);
-  return fieldNames;
-};
+    // const registered = this.constructor.fieldNames()
+    // const viewRegistered = this.constructor.viewFieldNames()
+    // const diff = registered.filter(fieldName => !viewRegistered.includes(fieldName));
+    // fieldNames = fieldNames.concat(diff)
+    // logger.keyValue('registered', registered)
+    // logger.keyValue('viewRegistered', viewRegistered)
+    // logger.keyValue('diff', diff)
+    logger.keyValue('fieldNames', fieldNames)
+    return fieldNames
+}
 
 /**
  * Валидация записи.
  */
 Model.prototype.$validate = function (fieldNames = null) {
-  return logger.methodCall(
-    `${this.$entityNameWithId}.$validate`,
-    arguments,
-    () => {
-      if (!fieldNames) fieldNames = this.$fieldNamesForValidate();
-      this.$clearErrors();
-      this.constructor.eachFields((field) => {
-        if (!(field instanceof VariableField || field instanceof Field)) return;
-        // console.warn(field.name, this[field.name])
+    return logger.methodCall(`${this.$entityNameWithId}.$validate`, arguments, () => {
+        if (!fieldNames) fieldNames = this.$fieldNamesForValidate()
+        this.$clearErrors()
+        this.constructor.eachFields(field => {
+            if (!(field instanceof VariableField || field instanceof Field)) return
+            // console.warn(field.name, this[field.name])
 
-        if (!field.isValid(this[field.name], this)) {
-          this.constructor.handleValidateError(field, field.error);
-          this.$errors.push(field.error);
-        }
+            if (!field.isValid(this[field.name], this)) {
+                this.constructor.handleValidateError(field, field.error)
+                this.$errors.push(field.error)
+            }
 
-        // if (Array.isArray(field.value) && field.itemOf) {
-        //     this[field.name] = field.value = field.value.map(item => {
-        //         // field.itemOf.name = `${field.name} [${index}]`
-        //         if (!field.itemOf.isValid(item, this)) {
-        //             this.constructor.handleValidateError(field.itemOf, field.itemOf.error)
-        //             this.$errors.push(field.itemOf.error)
-        //         }
-        //         return field.itemOf.convertType(item)
-        //     })
-        // }
-      }, fieldNames);
+            // if (Array.isArray(field.value) && field.itemOf) {
+            //     this[field.name] = field.value = field.value.map(item => {
+            //         // field.itemOf.name = `${field.name} [${index}]`
+            //         if (!field.itemOf.isValid(item, this)) {
+            //             this.constructor.handleValidateError(field.itemOf, field.itemOf.error)
+            //             this.$errors.push(field.itemOf.error)
+            //         }
+            //         return field.itemOf.convertType(item)
+            //     })
+            // }
+        }, fieldNames)
 
-      this.constructor.eachRelations((relation) => {
-        [this[relation.name]].flat().forEach((relationEntity) => {
-          if (!relationEntity.$validate()) {
-            this.constructor.handleValidateError(
-              relation,
-              relationEntity.$errors
-            );
-            this.$errors.push(relationEntity.$errors);
-          }
-        });
-      });
-      return this.$errors.length < 1;
-    }
-  );
-};
+        this.constructor.eachRelations(relation => {
+            ;[this[relation.name]].flat().forEach(relationEntity => {
+                if (!relationEntity.$validate()) {
+                    this.constructor.handleValidateError(relation, relationEntity.$errors)
+                    this.$errors.push(relationEntity.$errors)
+                }
+            })
+        })
+        return this.$errors.length < 1
+    })
+}
 
 /**
  * Валидация записи/записей.
  */
 Model.validate = function (entity, fieldNames = null) {
-  if (Array.isArray(entity)) {
-    entity.forEach((_entity) => this.validate(_entity, fieldNames));
-  } else if (entity && entity instanceof this) {
-    return entity.$validate(fieldNames);
-  } else if (entity && "object" === typeof entity) {
-    return new this(entity).$validate(fieldNames);
-  }
-  console.warn(
-    `${this.entityName}.validate() argument 1` +
-      " must be an object or an array of the objects" +
-      `, given:`,
-    entity
-  );
-  return false;
-};
+    if (Array.isArray(entity)) {
+        entity.forEach(_entity => this.validate(_entity, fieldNames))
+    } else if (entity && entity instanceof this) {
+        return entity.$validate(fieldNames)
+    } else if (entity && 'object' === typeof entity) {
+        return new this(entity).$validate(fieldNames)
+    }
+    console.warn(
+        `${this.entityName}.validate() argument 1` +
+            ' must be an object or an array of the objects' +
+            `, given:`,
+        entity
+    )
+    return false
+}
